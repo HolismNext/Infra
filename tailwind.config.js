9dceba48dab1a9d88a415f0bd5c844d3c399cc51
“Sanam” <35029825+pourmand2010@users.noreply.github.com>
2021-10-20 00:18:12 +0330

    committed by script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './pages/**/*.{js,ts,jsx,tsx}', 
+    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './contents/**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          50: '#f8f3ed',
+          100: '#f4ebe2',
+          200: '#eadac8',
+          300: '#dac0a0',
+          400: '#c7a070',
+          450: '#bf925b',
+          500: '#ad7d43',
+          600: '#846033',
+          700: '#674b28',
+          800: '#584022',
+          900: '#46331b'
+        },
+      },
+    },
   },
   variants: {
     extend: {},
