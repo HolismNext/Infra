014b49c71680aea46f74b9a115ac9b74f3fcdac8
Saeed Nemati <sn.1361@gmail.com>
2021-11-06 18:18:59 +0330

    committed by script

--- Base/App.js
+++ Base/App.js
@@ -2,6 +2,7 @@ import EventManager from "./EventManager";
 import Validation from "./Validation";
 import Holism from "./Holism";
 import Globalization from './Globalization';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 const app = {
     ...EventManager,
