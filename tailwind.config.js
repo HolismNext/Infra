7dd55881dd809ca37cc666f106124b0a0cbd38a6
Saeed Nemati <sn.1361@gmail.com>
2021-11-08 15:56:53 +0330

    committed by script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,13 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}', 
     './components/**/*.{js,ts,jsx,tsx}',
     './contents/**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      'title-1': ['font-title-1'],
-      'body-1': ['font-body-1'],
-      'title-2': ['font-title-2'],
-      'body-2': ['font-body-2'],
-    },
-    extend: {
-      backgroundImage: {
-        'booking-1': "url('/images/booking1.jpeg')",
-        'owner-1': "url('/images/owner1.jpg')",
-        'stylist-1': "url('/images/stylist1.jpeg')",
-        'stylist-2': "url('/images/stylist2.jpg')",
-       },
-      colors: {
-        brown: {
-          100: 'var(--color-brown-100)',
-          200: 'var(--color-brown-200)',
-          300: 'var(--color-brown-300)',
-          450: 'var(--color-brown-450)',
-          700: 'var(--color-brown-700)',
-          900: 'var(--color-brown-900)',
-        },
-        gray: {
-          100: 'var(--color-gray-100)',
-          200: 'var(--color-gray-200)',
-          300: 'var(--color-gray-300)',
-          1100: 'var(--color-gray-1100)',
-          1200: 'var(--color-gray-1200)',
-        },
-
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
