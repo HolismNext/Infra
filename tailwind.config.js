139ce107b13f72f72c0a8f09597356c558d77646
Saeed Nemati <sn.1361@gmail.com>
2021-11-17 10:44:14 +0330

    committed by script

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}', 
     './components/**/*.{js,ts,jsx,tsx}',
+    './Base/**/*.{js,ts,jsx,tsx}',
+    './UiComponents/**/*.{js,ts,jsx,tsx}',
     './contents/**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
