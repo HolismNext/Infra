13e1eb7374132170e92bab307cd72bf9e161317a
Saeed Nemati <sn.1361@gmail.com>
2021-11-19 15:11:32 +0330

    committed by script

--- UiComponents/Navbar/One.js
+++ UiComponents/Navbar/One.js
@@ -9,7 +9,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar({ menuItems }) {
+export default function Navbar({ logo, brand, companyName, menuItems }) {
 
     const router = useRouter()
 
@@ -36,16 +36,18 @@ export default function Navbar({ menuItems }) {
                             </div>
                             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex-shrink-0 flex items-center">
-                                    <img
-                                        className="block lg:hidden h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                                        alt="Sadra123"
-                                    />
-                                    <img
-                                        className="hidden lg:block h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                                        alt="Sadra123"
-                                    />
+                                    <span className="block lg:hidden h-8 w-auto">
+                                        {logo}
+                                    </span>
+                                    <span>
+                                        <span className="hidden lg:block h-8 w-auto">
+                                            {logo}
+                                        </span>
+                                        <img
+                                            className="hidden lg:block h-8 w-auto"
+                                            src={brand}
+                                        />
+                                    </span>
                                 </div>
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
