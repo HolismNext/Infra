7229c58e7abeea967a87c2265838467eb3702dbe
Saeed Nemati <sn.1361@gmail.com>
2021-11-22 15:03:11 +0330

    committed by script

--- UiComponents/Features/One.js
+++ UiComponents/Features/One.js
@@ -1,52 +1,35 @@
-import { Section, SectionTitle, SectionWrapper } from "./Styles";
-import { BadgeCheckIcon } from '@heroicons/react/solid'
+import SectionOne from '../Section/One';
+import { T, t, isRtl } from '../../Base/Globalization';
+import React from 'react';
 
-const featureStyle = "bg-gray-100 rounded-lg p-6 relative";
-const iconStyle = " w-12 h-12 bg-blue-700 text-white p-2 rounded-md absolute -top-6 right-0 left-0 m-auto";
-const titleStyle = "text-center text-lg font-bold text-gray-800 mt-4 mb-2";
-const descriptionStyle = "text-center text-md text-gray-600 ";
+const FeaturesOne = ({
+    title,
+    subtitle,
+    description,
+    features
+}) => {
+    return <SectionOne>
+        <div className="text-blue-500 uppercase tracking-wider text-center"><T>{title}</T></div>
+        <div className="text-xl font-bold tracking-wide text-gray-900 text-center my-4"><T>{subtitle}</T></div>
+        <div className="text-sm text-gray-700 text-center mb-12"><T>{description}</T></div>
 
-const FeaturesOne = () => {
-    return <div className={Section}>
-        <div className={SectionWrapper}>
-            <div className="text-blue-500 uppercase tracking-wider text-center">Perform more</div>
-            <div className="text-xl font-bold tracking-wide text-gray-900 text-center my-4">Use more tools, to get more done.</div>
-            <div className="text-sm text-gray-700 text-center mb-12">We have a variety of services to offer. Let Sadra be a tool in your toolset. Trust the new wave of technology to help you perform your business better.</div>
+        <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-3">
 
-            <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-3">
-                <div className={featureStyle}>
-                    <BadgeCheckIcon className={iconStyle} />
-                    <div className={titleStyle}>Title one</div>
-                    <div className={descriptionStyle}>This is a sample description for feature. This needs to be the same in length.</div>
-                </div>
-                <div className={featureStyle}>
-                    <BadgeCheckIcon className={iconStyle} />
-                    <div className={titleStyle}>Title one</div>
-                    <div className={descriptionStyle}>This is a sample description for feature. This needs to be the same in length.</div>
-                </div>
-                <div className={featureStyle}>
-                    <BadgeCheckIcon className={iconStyle} />
-                    <div className={titleStyle}>Title one</div>
-                    <div className={descriptionStyle}>This is a sample description for feature. This needs to be the same in length.</div>
-                </div>
-                <div className={featureStyle}>
-                    <BadgeCheckIcon className={iconStyle} />
-                    <div className={titleStyle}>Title one</div>
-                    <div className={descriptionStyle}>This is a sample description for feature. This needs to be the same in length.</div>
-                </div>
-                <div className={featureStyle}>
-                    <BadgeCheckIcon className={iconStyle} />
-                    <div className={titleStyle}>Title one</div>
-                    <div className={descriptionStyle}>This is a sample description for feature. This needs to be the same in length.</div>
-                </div>
-                <div className={featureStyle}>
-                    <BadgeCheckIcon className={iconStyle} />
-                    <div className={titleStyle}>Title one</div>
-                    <div className={descriptionStyle}>This is a sample description for feature. This needs to be the same in length.</div>
-                </div>
-            </div>
+            {
+                features.map((feature, index) => {
+                    const clonedIcon = React.cloneElement(feature.icon, {
+                        className: " w-12 h-12 bg-blue-700 text-white p-2 rounded-md absolute -top-6 right-0 left-0 m-auto"
+                    });
+                    return <div index={index} className="bg-gray-100 rounded-lg p-6 relative">
+                        {clonedIcon}
+                        <div className="text-center text-lg font-bold text-gray-800 mt-4 mb-2"><T>{feature.title}</T></div>
+                        <div className="text-center text-md text-gray-600 "><T>{feature.description}</T></div>
+                    </div>
+                }
+                )
+            }
         </div>
-    </div>
+    </SectionOne>
 }
 
 export default FeaturesOne;
