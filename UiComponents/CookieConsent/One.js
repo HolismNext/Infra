98d5e2f356c02576adcfd67b7a831f394f41bbbf
Saeed Nemati <sn.1361@gmail.com>
2021-12-04 09:05:34 +0330

    committed by script

--- UiComponents/CookieConsent/One.js
+++ UiComponents/CookieConsent/One.js
@@ -1,5 +1,12 @@
+import Script from 'next/script'
+
 const CookieConsentOne = () => {
-    return <div>Do you agree to our cookies?</div>
+    return <>
+        <div>Do you agree to our cookies?</div>
+        <Script id='cookieConsentScript' strategy='beforeInteractive'>
+
+        </Script>
+    </>
 }
 
 export default CookieConsentOne;
