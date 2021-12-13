ea7e7885974a8bfbd0e0eb4eccda60b8bf6ab3b1
“Sanam” <pourmand2010@users.noreply.github.com>
2021-12-13 13:23:59 +0330

    committed by script

--- UiComponents/Navbar/Two.js
+++ UiComponents/Navbar/Two.js
@@ -6,6 +6,9 @@ import T, { isRtl, hasLocales } from '../../Base/Globalization'
 import { useState } from "react";
 
 
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 const NavbarTwo = ({
     logo,
     logoStyles,
@@ -34,25 +37,54 @@ const NavbarTwo = ({
         <div onClick={() => changeLocale('en')} className={locale === 'en' ? selectedLocaleStyle : localeStyle}>En</div>
         <div onClick={() => changeLocale('fa')} className={locale === 'fa' ? selectedLocaleStyle : localeStyle}>فا</div>
     </div>
-
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(true);
     return <>
         {open ?
-            <> </>
+            <>
+             <div className="sm:hidden">
+                        <div className="px-2 pt-2 pb-3 space-y-1">
+                            {menuItems.map((item) => (
+                                <Link
+                                    key={item.name}
+                                    href={item.href}
+                                >
+                                    <a
+                                        className={classNames(
+                                            item.href === router.pathname ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block px-3 py-2 rounded-md text-base font-medium select-none'
+                                        )}
+                                        aria-current={item.href === router.pathname ? 'page' : undefined}
+                                    >
+                                        <T>{item.name}</T>
+                                    </a>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+             </>
             :
             <>
                 <div className="flex mx-4 mt-2 font-sans">
                     <div className="flex-none text-purple-900">
                         <img
+                            className="h-8 w-auto"
                             src={logo}
                             alt={companyName} />
                     </div>
                     <div className="flex-1 text-gray-500 text-sm  md:text-lg">
                         <div className="flex justify-center	">
-                            <div className="px-2">Product</div>
-                            <div className="px-2">Features</div>
-                            <div className="px-2">Marketplace</div>
-                            <div className="px-2">Company</div>
+                            {
+                                menuItems.map((item) => {
+                                    return <div className="px-2">
+                                        <Link key={item.name} href={item.href}>
+                                            <a className="px-2" >
+                                                <T>{item.name}</T>
+                                            </a>
+                                        </Link>
+                                    </div>
+                                })
+                            }
+
                         </div>
                     </div>
                     <div className="flex-none text-right">
