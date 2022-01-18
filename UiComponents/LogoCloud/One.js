c5c0cf0595e80c2b6342282f0716aa95f6094edc
Saeed Nemati <sn.1361@gmail.com>
2022-01-18 10:34:50 +0330

    committed by script

--- UiComponents/LogoCloud/One.js
+++ UiComponents/LogoCloud/One.js
@@ -11,10 +11,10 @@ const LogoCloudOne = ({
     >
         {
             brands && brands.map && brands.length > 0 &&
-            <div>
+            <div class="flex flex-wrap gap-3 mt-12 justify-center">
                 {
-                    brands.map(brand => <div key={brand.name}>
-                        <div>{brand.logo}</div>
+                    brands.map(brand => <div key={brand.name} className="flex p-3 bg-white rounded-lg items-center gap-3">
+                        <div className="w-12 h-12 relative flex items-center justify-center"><img className="object-cover" src={brand.logo} alt={brand.name + ' Logo'} /></div>
                         <div>{brand.name}</div>
                     </div>)
                 }
