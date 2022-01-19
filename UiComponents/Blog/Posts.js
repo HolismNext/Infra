f79fe4538db9535bae9f3843ede53bef034ac1c4
Saeed Nemati <sn.1361@gmail.com>
2022-01-19 13:19:33 +0330

    committed by script

--- UiComponents/Blog/Posts.js
+++ UiComponents/Blog/Posts.js
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 
-const Posts = ({ posts }) => {
+const Posts = ({
+    posts,
+
+}) => {
     /*
     title
     categories, tags
@@ -10,14 +13,18 @@ const Posts = ({ posts }) => {
     x min read
     
     */
-    return <div>
+    return <div className=" md:grid md:grid-cols-2 gap-4 xl:grid-cols-3 xl:gap-6 xl:my-12">
         {
             Object.keys(posts).map(key => {
                 const post = posts[key];
                 const url = key.startsWith('/') ? key : `/${key}`;
-                return <div key={key}>
-                    <Link href={url}><a className="text-red-400">{post.title}</a></Link>
-                    {post.date}
+                return <div key={key} className="bg-white border-b md:border-b-0 p-6 xl:h-48 xl:rounded-md">
+                    <div>
+                        <Link href={url}>
+                            <a className="prose text-2xl">{post.title}</a>
+                        </Link>
+                        <div className="text-sm text-slate-400">{post.date}</div>
+                    </div>
                 </div>
             })
         }
