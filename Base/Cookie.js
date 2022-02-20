9db76ea9eb44f5aa4d77d2b034ed039d99e4c712
Saeed Nemati <sn.1361@gmail.com>
2022-02-20 08:05:40 +0330

    committed by script

--- Base/Cookie.js
+++ Base/Cookie.js
@@ -6,12 +6,18 @@ const Cookie = {
             .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
         return cookies[key];
     },
-    setCookie: (key, value) => {
-        document.cookie = `${key}=${value}`;
+    setCookie: (key, value, days) => {
+        let expires = '';
+        if (days) {
+            expires = new Date();
+            expires.setDate(expires.getDate() + days);
+            expires = `; expires=${expires.toUTCString()}`;
+        }
+        document.cookie = `${key}=${value}${expires}; domain=${location.hostname}`;
     },
-    consentKey: 'hasConsent',
+    consentKey: 'hasCookieConsent',
     setConsent: () => {
-        Cookie.setCookie(Cookie.consentKey, true);
+        Cookie.setCookie(Cookie.consentKey, true, 100);
     },
     hasConsent: () => {
         return Cookie.getCookie(Cookie.consentKey) === 'true';
---
+++ UiUx/Image.md
@@ -0,0 +1,8 @@
+# Image UI/UX
+
+### Options
+- Hover backdrop + text visibility + image scale
+    - On hover
+        - Show a backdrop using animation
+        - Meanwhile scale the image
+        - Then show the text using animation
