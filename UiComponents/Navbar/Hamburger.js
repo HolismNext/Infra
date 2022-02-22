614efcf84366346f53d4a146b44e359d6de835d3
Saeed Nemati <sn.1361@gmail.com>
2022-02-22 11:25:01 +0330

    committed by script

--- UiComponents/Navbar/Hamburger.js
+++ UiComponents/Navbar/Hamburger.js
@@ -1,14 +1,15 @@
+//https://tailwindcomponents.com/component/wip-hamburger-menu-animation
 import { useState, useEffect } from 'react'
 
 const Hamburger = ({
-    className,
+    containerStyle,
     stickStyle,
     onToggle
 }) => {
 
     const [isOpen, setIsOpen] = useState(false)
 
-    const stickClass = "w-5 h-[3px] transition duration-500 ease-in-out " + (stickStyle || '')
+    const stickClass = `block absolute transform transition duration-300 ease-in-out ${stickStyle || ''} `
 
     useEffect(() => {
         if (onToggle && typeof onToggle === 'function') {
@@ -16,13 +17,13 @@ const Hamburger = ({
         }
     }, [isOpen])
 
-    return <div className={className || 'w-10 h-10'}>
-        <button className="text-gray-500 w-full h-full relative focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
-            {/* <span className="sr-only">Open main menu</span> */}
-            <div className="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <span aria-hidden="true" className="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" className={isOpen ? 'rotate-45' : ' -translate-y-1.5'}></span>
-                <span aria-hidden="true" className="block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out" className={isOpen ? 'opacity-0' : ''}></span>
-                <span aria-hidden="true" className="block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out" className={isOpen ? '-rotate-45' : ' translate-y-1.5'}></span>
+    return <div className={containerStyle || 'w-full h-full'}>
+        <button className="w-full h-full relative focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
+            <span className="sr-only">Open main menu</span>
+            <div className="block w-full h-full flex flex-col bg-yellow-400">
+                <span aria-hidden="true" className={stickClass + (isOpen ? "rotate-45" : "-translate-y-1.5")}></span>
+                <span aria-hidden="true" className={stickClass + (isOpen && "opacity-0")}></span>
+                <span aria-hidden="true" className={stickClass + (isOpen ? "-rotate-45" : "translate-y-1.5")}></span>
             </div>
         </button>
     </div>
