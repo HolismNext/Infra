903805ff012aba8d8c8629309de388a1bc60836e
Saeed Nemati <sn.1361@gmail.com>
2022-07-24 13:00:23 +0430

    committed by script

--- Base/useSection.js
+++ Base/useSection.js
@@ -3,6 +3,8 @@ import SiteContext from './SiteContext'
 
 const useSection = (key) => {
 
+    const temp = { items: [] }
+
     if (!key) {
         // throw new Error('Key is not provided to extract the section data from API')
     }
@@ -11,16 +13,18 @@ const useSection = (key) => {
 
     if (sections == null) {
         // throw new Error('No sections in the API')
+        return temp
     }
 
     if (!Array.isArray(sections)) {
         // throw new Error('Sections is not an array')
+        return temp
     }
 
     const section = sections.find(i => i.key && i.key.toLowerCase() === key.toLowerCase())
     if (!section) {
         // throw new Error(`${key} does not exist in the API`)
-        return []
+        return temp
     }
 
     return section
